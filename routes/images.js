b0e46dd2e85cccf61be2270aa646415491e36f02
Emil <emil.emanuel@hotmail.com>
2018-02-19 11:34:04 +0100

    added database helper and some refactoring

--- routes/images.js
+++ routes/images.js
@@ -4,7 +4,7 @@ let router = require('express').Router()
 
 router.route('/')
   .get((req, res) => {
-    res.render('images/images')
+    res.render('images/images', { Image: image.all() })
   })
 
 module.exports = router
