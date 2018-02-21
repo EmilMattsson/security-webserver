02c908bf09d41bf3115d7f43c95ffd1b9fbdeb69
Emil <emil.emanuel@hotmail.com>
2018-02-21 12:13:33 +0100

    authentification

--- routes/user.js
+++ routes/user.js
@@ -4,29 +4,41 @@ let router = require('express').Router()
 let User = require('../models/User')
 let bcrypt = require('bcrypt')
 
-router.route('/register')
+router.route('/login')
   .get((req, res) => {
-    res.render('user/register')
+    res.render('user/login')
   })
   .post((req, res) => {
     if (req.body.username && req.body.password) {
-      let user = new User({
+      let unauthenticatedUser = new User({
         username: req.body.username,
         password: req.body.password
       })
       bcrypt.hash(req.body.password, 10).then((hash) => {
         user.password = hash
-        console.log(user)
-        user.save().then(() => {
-          res.redirect('/')
-        }).catch(err => {
-          console.log(err.message)
-          response.render('user/register', {error: "Username already taken!"})
-        })
+        unauthenticatedUser.findOne({ username: user.username })
+          .exec((err, user) => {
+            console.log(user)
+            if (err) {
+              res.render('error/401')
+            } else if (!user) {
+              res.render('error/500')
+            }
+            bcrypt.hash(user.password, 10).then((hash) => {
+              user.password = hash
+              bcrypt.compare(unauthenticatedUser.password, user.password, (err, result) => {
+                if (result === true) {
+                  res.redirect('/images')
+                } else {
+                  res.render('error/401')
+                }
+              })
+            })
+          })
       })
 
     } else {
-      res.render('user/register', {error: 'You must enter a username and a password'})
+      res.render('user/login', {error: 'You must enter a username and a password'})
     }
   })
 
---
+++ routes/user/register.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let router = require('express').Router()
+let User = require('../models/User')
+let bcrypt = require('bcrypt')
+
+router.route('/register')
+  .get((req, res) => {
+    res.render('user/register')
+  })
+  .post((req, res) => {
+    if (req.body.username && req.body.password) {
+      let user = new User({
+        username: req.body.username,
+        password: req.body.password
+      })
+      bcrypt.hash(req.body.password, 10).then((hash) => {
+        user.password = hash
+        console.log(user)
+        user.save().then(() => {
+          res.redirect('/')
+        }).catch(err => {
+          console.log(err.message)
+          response.render('user/register', {error: "Username already taken!"})
+        })
+      })
+
+    } else {
+      res.render('user/register', {error: 'You must enter a username and a password'})
+    }
+  })
+
+module.exports = router
