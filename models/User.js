b416872dec2279adf1057c49623906bce92cbb33
KungenB <eb222sv@student.lnu.se>
2018-02-21 10:07:10 +0100

    remove of mail

--- models/User.js
+++ models/User.js
@@ -5,12 +5,6 @@ let bcrypt = require('bcrypt')
 
 // Defining schema
 let UserSchema = mongoose.Schema({
-  // email: {
-  //   type: String,
-  //   unique: true,
-  //   required: true,
-  //   trim: true
-  // },
   username: {
     type: String,
     unique: true,
